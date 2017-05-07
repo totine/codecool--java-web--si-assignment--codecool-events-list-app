4600b68b8a06564617d49b2c2961d126c3fd861e
totine <joanna.gargas@gmail.com>
2017-05-07 23:26:18 +0200

    Add EventCategory class in model. Define attributes in Event class

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -1,4 +1,13 @@
 package model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Event {
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String url;
+    private EventCategory category;
 }
