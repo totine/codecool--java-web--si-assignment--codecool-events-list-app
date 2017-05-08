dc13b3d08185099e19111ec4479968801b7831a2
totine <joanna.gargas@gmail.com>
2017-05-08 20:52:22 +0200

    Create Event constructors, getters and setters

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -4,10 +4,75 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Event {
+    private Integer id;
     private String name;
     private LocalDate date;
     private LocalTime time;
     private String description;
     private String url;
     private EventCategory category;
+
+    public Event() {
+
+    }
+
+    public Event(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public EventCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(EventCategory category) {
+        this.category = category;
+    }
 }
