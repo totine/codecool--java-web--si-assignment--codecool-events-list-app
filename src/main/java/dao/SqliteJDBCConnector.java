5bd324d24eeb9484d628e4378ff25ac237b7a01a
totine <joanna.gargas@gmail.com>
2017-05-09 14:41:23 +0200

    Add SqliteJDBCConnector to make connect with database. Add sqlite-jdbc dependency in pom.xml

---
+++ src/main/java/dao/SqliteJDBCConnector.java
@@ -0,0 +1,20 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class SqliteJDBCConnector {
+    public static Connection connection() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
+        } catch (SQLException e) {
+            System.out.println("Connection to DB failed");
+        }
+
+        return connection;
+    }
+}
