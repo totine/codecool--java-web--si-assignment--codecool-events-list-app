ddedb5116d2b35d655a2aa1c9ba3b9618e9a77ac
totine <joanna.gargas@gmail.com>
2017-05-09 22:33:41 +0200

    Add constructor, getters and setters scheme in event category model

--- src/main/java/model/EventCategory.java
+++ src/main/java/model/EventCategory.java
@@ -2,4 +2,30 @@ package model;
 
 
 public class EventCategory {
+    private Integer id;
+    private String name;
+
+    public EventCategory(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
 }
