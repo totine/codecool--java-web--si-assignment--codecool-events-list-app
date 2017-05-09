a0b0cc18049062ca66500e0ec1ddff204bde5dd9
totine <joanna.gargas@gmail.com>
2017-05-09 14:32:29 +0200

    Create EventDaoSqlite, that implements EventDao interface

---
+++ src/main/java/dao/EventDaoSqlite.java
@@ -0,0 +1,34 @@
+package dao;
+
+import model.Event;
+import model.EventCategory;
+
+import java.util.List;
+
+
+public class EventDaoSqlite implements EventDao {
+    @Override
+    public void add(Event event) {
+
+    }
+
+    @Override
+    public Event find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Event> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Event> getBy(EventCategory eventCategoryCategory) {
+        return null;
+    }
+}
