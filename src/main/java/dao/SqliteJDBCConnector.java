1ebd4fa0e6ab21bd7c8450f3c592273e877add7f
totine <joanna.gargas@gmail.com>
2017-05-09 22:29:43 +0200

    Change database path in SqliteJDBCConnector

--- src/main/java/dao/SqliteJDBCConnector.java
+++ src/main/java/dao/SqliteJDBCConnector.java
@@ -10,7 +10,7 @@ public class SqliteJDBCConnector {
         Connection connection = null;
 
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/database.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/data/database.db");
         } catch (SQLException e) {
             System.out.println("Connection to DB failed");
         }
