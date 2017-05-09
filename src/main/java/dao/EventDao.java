c37c43902e075ee8d21fac060c1f9ca34e33ed5f
totine <joanna.gargas@gmail.com>
2017-05-09 14:30:12 +0200

    Add method scheme to EventDao interface

--- src/main/java/dao/EventDao.java
+++ src/main/java/dao/EventDao.java
@@ -1,7 +1,16 @@
 package dao;
 
-/**
- * Created by rafalstepien on 28/04/2017.
- */
-public class EventDao {
+import model.Event;
+import model.EventCategory;
+
+import java.util.List;
+
+public interface EventDao {
+
+    void add(Event event);
+    Event find(int id);
+    void remove(int id);
+
+    List<Event> getAll();
+    List<Event> getBy(EventCategory eventCategoryCategory);
 }
