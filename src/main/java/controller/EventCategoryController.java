4226b02bd50bc57704c6da7aa440a86cbfeb4f0a
totine <joanna.gargas@gmail.com>
2017-05-14 14:09:36 +0200

    Improve adding category - cannot add category with the duplicate name

--- src/main/java/controller/EventCategoryController.java
+++ src/main/java/controller/EventCategoryController.java
@@ -16,12 +16,24 @@ public class EventCategoryController {
 
     public static ModelAndView renderEventCategoryAdd(Request req, Response res) {
         Map params = new HashMap<>();
+        if (req.session().attribute("category_to_flash")!=null)
+
+            params.put("categoryToFlash", req.session().attribute("categoryToFlash"));
+
         return new ModelAndView(params, "category/form");
     }
 
     public static String addNewEventCategory(Request req, Response res) {
+        if (eventCategoryDao.find(req.queryMap("category_name").value().toLowerCase().trim())!=null) {
+
+            res.redirect("/category/add");
+            req.session().attribute("categoryToFlash", req.queryMap("category_name").value());
+            System.out.println(req.session().attributes());
+            return null;
+        }
         EventCategory newEventCategory = new EventCategory(req.queryMap("category_name").value());
         eventCategoryDao.add(newEventCategory);
+
         res.redirect("/event/add");
         return null;
     }
